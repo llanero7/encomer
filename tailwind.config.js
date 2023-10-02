b31b145da6c835123f0a1f7533966535587e5212
Rafael <llanero7@gmail.com>
2023-10-02 10:46:32 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue:'#A6C8E7',
+        darkBlue: '#00203A',
+        medBlue: '#005570',
+        ligthGreen: '#00A39F',
+        ligtherGreen: '#3FB6B3',
+        ligtherGreenPlus: '#60CFCD', 
+        wornGreen: '#E3F5F5',
+        orange: '#FF4632',
+      },
+    },
+  },
+  plugins: [],
+}
+
