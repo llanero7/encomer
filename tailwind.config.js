2dfe8da60df296d37031b5884a205549e6223ada
Rafael <llanero7@gmail.com>
2023-10-17 10:06:23 -0400

    adding styles library

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,62 @@ module.exports = {
         lightBlue:'#A6C8E7',
         darkBlue: '#00203A',
         medBlue: '#005570',
-        ligthGreen: '#00A39F',
-        ligtherGreen: '#3FB6B3',
-        ligtherGreenPlus: '#60CFCD', 
+        lightGreen: '#00A39F',
+        lighterGreen: '#3FB6B3',
+        lighterGreenPlus: '#60CFCD', 
         wornGreen: '#E3F5F5',
         orange: '#FF4632',
+        primary: '#00A39F',
+        primaryLight: '#3FB6B3',
+        secondary: '#005570',
+        secondaryLight: '#23728B',
+        success: '#FF4632',
+        danger: '#DD6659',
+        dangerLight: '#E17A6F',
+        warning: '#EDC562',
+        warningLight: '#EDC562',
+        info: '#5086EC',
+        infoLight: '#73A3FE',
+        
+      },
+      fontFamily: {
+        'gilroyBold': ['gilroyBold', 'sans-serif',],
+        'gilroySemibold': ['gilroySemibold', 'sans-serif',],
+        'gilroyMedium': ['gilroyMedium', 'sans-serif',],
+        'gilroyRegular': ['gilroyRegular', 'sans-serif',],
+      }
+    },
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue:'#A6C8E7',
+        darkBlue: '#00203A',
+        medBlue: '#005570',
+        lightGreen: '#00A39F',
+        lighterGreen: '#3FB6B3',
+        lighterGreenPlus: '#60CFCD', 
+        wornGreen: '#E3F5F5',
+        orange: '#FF4632',
+        primary: '#00A39F',
+        primaryLight: '#3FB6B3',
+        secondary: '#005570',
+        secondaryLight: '#23728B',
+        success: '#50AC7E',
+        successLight: '#5BC08E',
+        danger: '#DD6659',
+        dangerLight: '#E17A6F',
+        warning: '#EDC562',
+        warningLight: '#F3D280',
+        info: '#5086EC',
+        infoLight: '#73A3FE',
+        
       },
       fontFamily: {
         'gilroyBold': ['gilroyBold', 'sans-serif',],
